bac7a5ee236174bcfbfb293ae128863bf56aa67b
Sai Tanush <saitanush789@gmail.com>
2025-10-14 22:02:53 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">React+Typescript+Tailwind Boilerplate</div>
+  )
+}
+
+export default App
